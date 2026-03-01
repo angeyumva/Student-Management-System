0f03e57e71d6345bcebb0feab35b67b49c6fb5d3
Igiraneza  kevin <207916721+Kevin-igiraneza@users.noreply.github.com>
2026-03-01 12:51:35 +0200

    Update Student.java with new implementation

--- src/Student.java
+++ src/Student.java
@@ -3,10 +3,16 @@ public class Student extends Person implements DatabaseOperations {
     private String course;
     private double marks;
 
-    public Student(int id, String course, double marks) {
+    public Student(int id, String name, String email, String course, double marks) {
+        super(name, email);
         this.id = id;
         this.course = course;
         this.marks = marks;
+    }
+
+    public Student(int id, String name, String email) {
+        super(name, email);
+        this.id = id;
     }
 
     public int getId() {
@@ -17,9 +23,57 @@ public class Student extends Person implements DatabaseOperations {
         return course;
     }
 
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
     public double getMarks() {
         return marks;
     }
 
-    // Implement methods from DatabaseOperations interface
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Student ID: " + id + ", Name: " + name + ", Email: " + email + ", Course: " + course + ", Marks: " + marks);
+    }
+
+    @Override
+    public void add(Object obj) {
+        // Implementation will be in StudentDAO
+    }
+
+    @Override
+    public void delete(int id) {
+        // Implementation will be in StudentDAO
+    }
+
+    @Override
+    public void update(Object obj) {
+        // Implementation will be in StudentDAO
+    }
+
+    @Override
+    public Object search(int id) {
+        // Implementation will be in StudentDAO
+        return null;
+    }
 }
