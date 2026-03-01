fbaa6f11bfd22b5dee619a16b6ca869b049469db
Igiraneza  kevin <207916721+Kevin-igiraneza@users.noreply.github.com>
2026-03-01 12:47:46 +0200

    Create StudentManagementApp.java with main application entry point

---
+++ src/StudentManagementApp.java
@@ -0,0 +1,9 @@
+package src;
+
+public class StudentManagementApp {
+    public static void main(String[] args) {
+        // Initialize the login page
+        LoginPage loginPage = new LoginPage();
+        loginPage.show();
+    }
+}
